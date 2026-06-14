dc1f5b304e3d96f7492a04f40b0318c05e8f9d22
hombach <christian@homba.ch>
2026-06-14 21:26:52 +0200

    cleanup

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,12 +1,26 @@
-"use strict";
+/**
+ * This is a dummy TypeScript test file using chai and mocha
+ *
+ * It's automatically excluded from npm and its build output is excluded from both git and npm.
+ * It is advised to test all your modules with accompanying *.test.ts-files
+ */
 
 import { expect } from "chai";
+import "chai/register-should.js"; // Extends Object.prototype with 'should'
+// import { functionToTest } from "./moduleToTest";
 
 describe("module to test => function to test", () => {
+	// initializing logic
 	const expected = 5;
 
 	it(`should return ${expected}`, () => {
 		const result = 5;
+		// assign result a value from functionToTest
 		expect(result).to.equal(expected);
+		// or using the should() syntax
+		result.should.be.equal(expected);
 	});
+	// ... more tests => it
 });
+
+// ... more test suites => describe
